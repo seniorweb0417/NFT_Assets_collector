e8cd9b1851e1706f35105abf48a85a8cccc2597c
seniorweb <seniorweb0417@outlook.com>
2022-06-03 16:45:13 +0200

    built skeleton and ui

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>NFT Assets to image</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="text-center">NFT Assets to PSD Generator</h1>
+            <form class="form-inline justify-content-center" action="">
+                <label for="contract_address" class="mr-sm-2 mb-2">Contract Address:</label>
+                <input type="text" class="form-control mb-2 mr-sm-2"  placeholder="Enter Contract address" id="contract_address">
+                <label for="pwd" class="mr-sm-2 mb-2">Token Id:</label>
+                <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Enter Token id" id="token_id">
+                <button type="submit" class="btn btn-primary mr-sm-2 mb-2">Get Info</button>
+                <button type="submit" class="btn btn-primary mb-2">Download</button>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/script.js"></script>
+    </body>
+</html>
